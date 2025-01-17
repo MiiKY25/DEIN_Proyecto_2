6a7e04deab5775f33bbd0274d23d776f04690cb0
mikel <mikel.ra.2003@gmail.com>
2025-01-17 13:36:11 +0100

    add:ventana acabada con ids y con su controlador

---
+++ src/main/java/org/mikel/dein_proyecto_2/controladores/bibliotecaController.java
@@ -0,0 +1,132 @@
+package org.mikel.dein_proyecto_2.controladores;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class bibliotecaController {
+
+    @FXML
+    private TableColumn<?, ?> colAlumnoApellido1;
+
+    @FXML
+    private TableColumn<?, ?> colAlumnoApellido2;
+
+    @FXML
+    private TableColumn<?, ?> colAlumnoDni;
+
+    @FXML
+    private TableColumn<?, ?> colAlumnoNombre;
+
+    @FXML
+    private TableColumn<?, ?> colHistoricoAlumno;
+
+    @FXML
+    private TableColumn<?, ?> colHistoricoDevolucion;
+
+    @FXML
+    private TableColumn<?, ?> colHistoricoFecha;
+
+    @FXML
+    private TableColumn<?, ?> colHistoricoID;
+
+    @FXML
+    private TableColumn<?, ?> colHistoricoLibro;
+
+    @FXML
+    private TableColumn<?, ?> colLibroAutor;
+
+    @FXML
+    private TableColumn<?, ?> colLibroBaja;
+
+    @FXML
+    private TableColumn<?, ?> colLibroCodigo;
+
+    @FXML
+    private TableColumn<?, ?> colLibroEditorial;
+
+    @FXML
+    private TableColumn<?, ?> colLibroEstado;
+
+    @FXML
+    private TableColumn<?, ?> colLibroImagen;
+
+    @FXML
+    private TableColumn<?, ?> colLibroTitulo;
+
+    @FXML
+    private TableColumn<?, ?> colPrestamoAlumno;
+
+    @FXML
+    private TableColumn<?, ?> colPrestamoFecha;
+
+    @FXML
+    private TableColumn<?, ?> colPrestamoID;
+
+    @FXML
+    private TableColumn<?, ?> colPrestamoLibro;
+
+    @FXML
+    private TableView<?> tablaAlumno;
+
+    @FXML
+    private TableView<?> tablaHistorico;
+
+    @FXML
+    private TableView<?> tablaLibro;
+
+    @FXML
+    private TableView<?> tablaPrestamo;
+
+    @FXML
+    void accionAcercaDe(ActionEvent event) {
+
+    }
+
+    @FXML
+    void accionAniadirAlumno(ActionEvent event) {
+
+    }
+
+    @FXML
+    void accionAniadirLibro(ActionEvent event) {
+
+    }
+
+    @FXML
+    void accionAyuda(ActionEvent event) {
+
+    }
+
+    @FXML
+    void accionAñadirHistoricoPrestamos(ActionEvent event) {
+
+    }
+
+    @FXML
+    void accionAñadirPrestamos(ActionEvent event) {
+
+    }
+
+    @FXML
+    void accionBajaLibro(ActionEvent event) {
+
+    }
+
+    @FXML
+    void accionEliminarAlumno(ActionEvent event) {
+
+    }
+
+    @FXML
+    void accionModificarAlumno(ActionEvent event) {
+
+    }
+
+    @FXML
+    void accionModificarLibro(ActionEvent event) {
+
+    }
+
+}
