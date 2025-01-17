1f2e2543e95aab5dfa7734a1d7ecf61c9b8734f2
mikel <mikel.ra.2003@gmail.com>
2025-01-17 09:47:24 +0100

    add:estructura de carpetas creada

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,10 @@
 module org.mikel.dein_proyecto_2 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
+    requires net.sf.jasperreports.core;
 
-
+    opens org.mikel.dein_proyecto_2.controladores to javafx.fxml;
     opens org.mikel.dein_proyecto_2 to javafx.fxml;
     exports org.mikel.dein_proyecto_2;
 }
