0123bf5c0cd315f5436a6f0c4d9eb4d9692d494d
mikel <mikel.ra.2003@gmail.com>
2025-01-17 12:29:06 +0100

    add:ventana principal hecha ,sin ids ni onactions

--- src/main/java/org/mikel/dein_proyecto_2/BibliotecaApplication.java
+++ src/main/java/org/mikel/dein_proyecto_2/BibliotecaApplication.java
@@ -3,6 +3,7 @@ package org.mikel.dein_proyecto_2;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,9 +11,12 @@ import java.io.IOException;
 public class BibliotecaApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(BibliotecaApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/biblioteca.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("LECTORIKA");
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("/imagenes/logo.svg")));
+        stage.setMinWidth(800);
+        stage.setMinHeight(500);
         stage.setScene(scene);
         stage.show();
     }
